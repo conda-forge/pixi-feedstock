7d71c9c382b9c6da6368af23ae947a6ed1f224da
Ruben Arts <ruben.arts@hotmail.com>
2024-06-05 11:46:24 +0200

    win: format env vars

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,19 +7,19 @@ REM
 
 REM Enable Link Time Optimization (LTO) for our release builds. This increases link time but drastically reduces
 REM binary size.
-set CARGO_PROFILE_RELEASE_LTO='true'
+set CARGO_PROFILE_RELEASE_LTO=true
 
 REM Use a single code gen unit, this effectively disables parallel linking but ensures that everything is linked
 REM together in a single unit which reduces the file-size at the cost of link time.
 REM Default for a release build is 16
-set CARGO_PROFILE_RELEASE_CODEGEN_UNITS='1'
+set CARGO_PROFILE_RELEASE_CODEGEN_UNITS=1
 
 REM Strip the binaries. This reduces the filesize of the final release.
-set CARGO_PROFILE_RELEASE_STRIP='symbols'
+set CARGO_PROFILE_RELEASE_STRIP=symbols
 
 REM Optimize the binary for size. This reduces the filesize at the cost of a slower binary.
 REM Options are 's' (size), 'z' (size and speed), '0-3' (speed, 0 being no optimization, 3 being max optimization)
-set CARGO_PROFILE_OPT_LEVEL='s'
+set CARGO_PROFILE_OPT_LEVEL=s
 
 
 set CARGO_HOME=D:\cargo
