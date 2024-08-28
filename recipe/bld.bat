a85f8172583e4859c01a3c228a2d257cbaa4a90e
Ruben Arts <ruben@prefix.dev>
2024-08-28 09:35:35 +0200

    set features for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 set CARGO_HOME=D:\cargo
-cargo install --locked --bins --root %PREFIX% --path .
+cargo install --locked --bins --root %PREFIX% --features native-tls --no-default-features --path .
 if %errorlevel% NEQ 0 exit /b %errorlevel%
 cargo-bundle-licenses --format yaml --output %SRC_DIR%/THIRDPARTY.yml
 if %errorlevel% NEQ 0 exit /b %errorlevel%
