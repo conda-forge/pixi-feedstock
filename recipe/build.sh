67d36059ba8d7c524f2232d7759510898e9d88b0
Ruben Arts <ruben.arts@hotmail.com>
2024-08-28 09:33:18 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export OPENSSL_DIR=$PREFIX
-cargo install --locked --bins --root ${PREFIX} --path .
+cargo install --locked --bins --root ${PREFIX} --features native-tls --no-default-features --path .
 cargo-bundle-licenses --format yaml --output ${SRC_DIR}/THIRDPARTY.yml
 rm $PREFIX/.crates2.json
 rm $PREFIX/.crates.toml
