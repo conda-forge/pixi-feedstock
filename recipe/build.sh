a09f4af514a2a74e6610338824321177b55a7f98
Ruben Arts <ruben.arts@hotmail.com>
2025-01-29 13:11:25 +0100

    add jemalloc and nushell script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+# If the linux-aarch64 and linux-ppc64le targets are present, set JEMALLOC_SYS_WITH_LG_PAGE to 16
 export OPENSSL_DIR=$PREFIX
 cargo install --locked --bins --root ${PREFIX} --path .
 cargo-bundle-licenses --format yaml --output ${SRC_DIR}/THIRDPARTY.yml
