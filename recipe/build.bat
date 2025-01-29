c49f3cd9a2902299d1d2a97ac560be1939ebeb95
Ruben Arts <ruben.arts@hotmail.com>
2025-01-29 13:18:27 +0100

    delete old script files

--- recipe/build.bat
+++
@@ -1,9 +0,0 @@
-set CARGO_HOME=D:\cargo
-cargo install --locked --bins --root %PREFIX% --path .
-if %errorlevel% NEQ 0 exit /b %errorlevel%
-cargo-bundle-licenses --format yaml --output %SRC_DIR%/THIRDPARTY.yml
-if %errorlevel% NEQ 0 exit /b %errorlevel%
-del %PREFIX%\.crates2.json
-if %errorlevel% NEQ 0 exit /b %errorlevel%
-del %PREFIX%\.crates.toml
-if %errorlevel% NEQ 0 exit /b %errorlevel%
